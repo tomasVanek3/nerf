47332bb40ebde6615eb70048a16ae74c59551aee
Tomas <l20220058@ssptaji.cz>
2024-12-18 18:03:48 +0100

    nerf

--- app.js
+++ app.js
@@ -20,35 +20,23 @@ function StranaNastaveni(){
     document.getElementById("second").className = "strana_viditelna";
 }
 
+let pocet_stisku = 0
+let pocet = 0;
 function policka(){
-    let checkboxy1 = document.querySelectorAll("vyber");
-    let checkboxy2 = document.querySelectorAll("vyber2");
-    let checkboxy3 = document.querySelectorAll("vyber3");
-    let pocet1 = 0;
-    let pocet2 = 0;
-    let pocet3 = 0;
-    checkboxy1.forEach(polozka => {
-        if (polozka.checked == false){
-            pocet1++;
-        }
-    });
-
-    checkboxy2.forEach(polozka => {
-        if (polozka.checked == false){
-            pocet2++
-        }
-    });
-
-    checkboxy3.forEach(element => {
-        if (polozka.checked == false){
-            pocet3
-        }
-    });
-
-    if (pocet1 > 1 || pocet2 > 1 || pocet3 > 1 || pocet1 == 0 || pocet2 == 0 || pocet3 == 0){
-        document.getElementById("p").innerText = "Neplatny vstup!";
+    pocet_stisku++;
+    //detekce kolikrat byl zmacknuty checkbox
+    if (pocet_stisku % 2 == 0){
+        pocet = 0;
     }
     else{
-        document.getElementById("p").innerText = "OK";
+        pocet++;
+    }
+    
+    if (pocet > 1 || pocet == 0){
+        document.getElementById("p").innerText = "Neplatny pocet";
+    }
+    else{
+        document.getElementById("p").innerText = "";
     }
 }
+
