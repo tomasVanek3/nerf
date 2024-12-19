cae633e75c0372f995f86ff6497fd54645e3d50f
Tomas <l20220058@ssptaji.cz>
2024-12-19 18:32:30 +0100

    nerf

--- app.js
+++ app.js
@@ -18,25 +18,110 @@ function StranaVybava(){
 function StranaNastaveni(){
     document.getElementById("main").className = "strana_vypnuta";
     document.getElementById("second").className = "strana_viditelna";
-}
-
-let pocet_stisku = 0
-let pocet = 0;
-function policka(){
-    pocet_stisku++;
-    //detekce kolikrat byl zmacknuty checkbox
-    if (pocet_stisku % 2 == 0){
-        pocet = 0;
-    }
-    else{
-        pocet++;
-    }
     
-    if (pocet > 1 || pocet == 0){
-        document.getElementById("p").innerText = "Neplatny pocet";
-    }
-    else{
-        document.getElementById("p").innerText = "";
-    }
 }
 
+let nul = 0;
+let i = 1;
+function Save(){
+    let hodnota = document.getElementById("select1").value;
+    let hodnota2 = document.getElementById("select2").value;
+    let hodnota3 = document.getElementById("select3").value
+    let obraz;
+    let ven;
+    if (hodnota == "Empty"){
+        nul = 1;
+        jakyJeToObrazek(hodnota, obraz, ven, nul);
+    }
+    else{
+        if (hodnota == "Infinus"){
+            nul = 0;
+            jakyJeToObrazek(hodnota, obraz, ven, nul);
+        }
+        else{
+            if (hodnota == "Titan"){
+                nul = 0;
+                jakyJeToObrazek(hodnota, obraz, ven, nul);
+            }
+        }
+    }
+
+    if (hodnota2 == "Empty"){
+        nul = 2;
+        jakyJeToObrazek(hodnota2, obraz, ven, nul);
+    }
+    else{
+        if (hodnota2 == "Stryfe"){
+            nul = 0;
+            jakyJeToObrazek(hodnota2, obraz, ven, nul);
+        }
+        else{
+            if (hodnota2 == "HyperFire"){
+                nul = 0;
+                jakyJeToObrazek(hodnota2, obraz, ven, nul);
+            }
+        }
+    }
+
+    if (hodnota3 == "Empty"){
+        nul = 3;
+        jakyJeToObrazek(hodnota3, obraz, ven, nul);
+    }
+    else{
+        if (hodnota3 == "FireStrike"){
+            nul = 0;
+            jakyJeToObrazek(hodnota3, obraz, ven, nul);
+        }
+        else{
+            if (hodnota3 == "Disruptor"){
+                nul = 0;
+                jakyJeToObrazek(hodnota3, obraz, ven, nul);
+            }
+        }
+    }
+}
+   
+
+
+function jakyJeToObrazek(hodnota, obraz, ven, nul){
+    obraz = "images/" + hodnota + ".png";
+    sessionStorage.setItem("key1", obraz);
+    ven = sessionStorage.getItem("key1");
+    if (nul == 0){
+        if (hodnota == "Infinus" || hodnota == "Titan"){
+            document.getElementById("obrazek1").src = ven;
+            document.getElementById("jmeno1").textContent = hodnota;
+        }
+        else{
+            if (hodnota == "Stryfe" || hodnota == "HyperFire"){
+                document.getElementById("obrazek2").src = ven;
+                document.getElementById("jmeno2").textContent = hodnota;
+            }
+            else{
+                if (hodnota == "FireStrike" || hodnota == "Disruptor"){
+                    document.getElementById("obrazek3").src = ven;
+                    document.getElementById("jmeno3").textContent = hodnota;
+                }
+            }
+        }
+    }
+    else{
+        if (nul == 1){
+            document.getElementById("obrazek1").src = ven;
+            document.getElementById("jmeno1").textContent = hodnota;
+        }
+        else{
+            if (nul == 2){
+                document.getElementById("obrazek2").src = ven;
+                document.getElementById("jmeno2").textContent = hodnota;
+            }
+            else{
+                if (nul == 3){
+                    document.getElementById("obrazek3").src = ven;
+                    document.getElementById("jmeno3").textContent = hodnota;
+                }
+            }
+        }
+    }
+    StranaVybava();
+}
